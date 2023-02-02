452a6cfb2cdb95db60f1ca34bf8b3e688ec6d61e
Noah McIvor <noahmcivor@gmail.com>
2023-02-02 17:21:08 -0500

    Removed unnecessary files and added a comment to test.cpp

--- test.cpp
+++ test.cpp
@@ -9,6 +9,10 @@ int main(){
 	int x = 1;
 	int y = 2;
 	std::cout << "Finally got Git to work!" << std::endl;
+	/* removed all the other files from staging and only will make chnges to test.cpp.
+	 * This is because I ran `git reset` and `git add test.cpp`, therefore only commiting 
+	 * test.cpp when I run `git commit`. 
+	 */
 
 	std::cout << "Works\n";
 }
