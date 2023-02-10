0cf3b56555eaa8b885c51d76e0a6ee5e4a160e0f
Noah McIvor <noahmcivor@gmail.com>
2023-02-10 16:57:30 -0500

    Setting GitHub Remote Repo Back to ur overwrite
    This reverts commit 8722bcaede44ba0f1d97762ea85c3ff18833dd90.

--- PP_0.cpp
+++ PP_0.cpp
@@ -1,199 +1,158 @@
-#include <cstddef>
 #include <iostream>
 #include <cstdlib>
 #include <string>
 #include <fstream>
 #include <iomanip>
 
+
 // Website node definition
 
 class Website{
-private:
-    std::string name;                         // Name of the website
-    std::string url;                          // URL of the website
-    std::string type;                      // Type of resource provided by the website
-    Website *next;                            // Next website in the list
-    Website *back;                            // Previous website in the list
+    public:
+        std::string name;                         // Name of the website
+        std::string url;                          // URL of the website
+        std::string type;                      // Type of resource provided by the website
+        Website(){};                              // Constructor with no arguments
+        Website(std::string new_name, std::string new_url, std::string new_type){         // Constructor with arguments
+            name = new_name;
+            url = new_url;
+            type = new_type;
+        }
+};
 
-public:
-    Website(){};                              // Constructor with no arguments
-    Website(std::string new_name, std::string new_url, std::string new_type, Website *n, Website *b){         // Constructor with arguments
-        name = new_name;
-        url = new_url;
-        type = new_type;
-        next = n;
-        back = b;
-    }
 
-    friend class LinkedList;
+// Node definition
+template <typename T>
+class Node{
+    private:
+        T data;
+        Node<T> *next;                            // Next website in the list
+        Node<T> *back;                            // Previous website in the list
+
+    public:
+        Node(){};                              // No argument constructor
+        Node(T new_data){                      // Constructor when website is known
+            data = new_data;
+            next = nullptr;
+            back = nullptr;
+        }
+    
+    template <typename E> friend class LinkedList;
 };
 
 // Linked List definition
+template <typename T>
 class LinkedList{
-private:
-    Website *header;
-    Website *trailer;
-    size_t size;
+    private:
+        Node<T> *header; 
+        Node<T> *trailer;
+        int size;
 
-public:
-    LinkedList(){                        // Linked List constructor
-        header = new Website;
-        trailer = new Website;
-        size = 0;
-        header->next = trailer;
-        trailer->back = header;
-    }
-
-    ~LinkedList(){                      // Linked List destructor
-        while(size != 0){
-            removeFront(); // size-- happens in removeFront() !!!!
+    public:
+        LinkedList(){                             // Linked List constructor 
+            header = new Node<T>;
+            trailer = new Node<T>;
+            size = 0;
+            header->next = trailer;
+            trailer->back = header;
         }
-        delete header;
-        delete trailer;
-    }
-    
-    int returnSize(){ return size; }
 
-    int isEmpty(){
-        if (header->next == trailer && trailer->back == header){
-           return 1; 
+
+        ~LinkedList(){                      // Linked List destructor
+            while(size != 0){
+                removeFront();
+            }
+            delete header;
+            delete trailer;
         }
-        return 0;
-    }
 
-    void remove(Website *toRemove){                  // Remove a given website from the list
-        if (!isEmpty()){
+        void remove(Node<T> *toRemove){                  // Remove a given website from the list
             toRemove->back->next = toRemove->next;
             toRemove->next->back = toRemove->back;
-            delete toRemove;
-            size--;
-        }
-        else {
-            std::cout << "There was an error accounting for the number of entries (size)!";
-            std::cout << std::endl;
         }
 
-    }
-
-    //accounts for empty list
-    void removeFront(){                              // Remove the first website
-        if (!isEmpty()){
+        void removeFront(){                              // Remove the first website
             remove(header->next);
-            /*
-            Website* temp = new Website;
-            temp->next = header->next;
-            header->next = header->next->next;
-            delete temp->next;
-            delete temp;
-            size--; // not needed in removeFront if accounted for in remove()
-            */
+            size--;
+            return;
         }
-        else {
-            std::cout << "List is already empty!" << std::endl;
-        }
-    }
 
-    void removeBack(){                               // Remove the last website
-        if (!isEmpty()){
+        void removeBack(){                               // Remove the last website
             remove(trailer->back);
-            /*
-            //Website* temp = new Website;
-            Website* temp = trailer->back;
-            //temp->back = trailer->back;
-            trailer->back = trailer->back->back;
-            //delete temp->back;
-            delete temp;
-            size--; // not needed in removeFront if accounted for in remove()
-
-            */
+            size--;
+            return;
         }
-        else {
-            std::cout << "List is already empty!" << std::endl;
+
+        void add(Node<T> *beforeMe, T data){    // General add function
+
+            // Create new website
+            Node<T> *newNode = new Node<T>(data);
+            
+            // Add website to linked list
+            beforeMe->back->next = newNode;
+            newNode->back = beforeMe->back;
+            beforeMe->back = newNode;
+            newNode->next = beforeMe;
+        
+            return;
         }
-        return;
-    }
 
-    void addBefore(Website *beforeMe, const std::string new_name, const std::string new_url, std::string new_type){    // General add function
+        void addFront(T data){   // Adding to the front of the list
+            add(header->next,data);
 
-        // Create new website
-        Website *newWebsite = new Website();
-        newWebsite->name = new_name;
-        newWebsite->url = new_url;
-        newWebsite->type = new_type;
+            size++;
+            return;
+        }
 
-        // Add website to linked list
-        beforeMe->back->next = newWebsite;
-        newWebsite->back = beforeMe->back;
-        beforeMe->back = newWebsite;
-        newWebsite->next = beforeMe;
-        size++; /* I *think* size needs to be increased at every addBefore() call,
-            but I could be wrong */
-    }
+        void addBack(T data){   // Adding to the end of the list
+            add(trailer,data);
 
-    void addFront(const std::string new_name, const std::string new_url, std::string new_type){   // Adding to the front of the list
-        addBefore(header->next,new_name,new_url,new_type);
-    }
+            size++;
+            return;
+        }
 
-    void addBack(const std::string new_name, const std::string new_url, std::string new_type){   // Adding to the end of the list
-        addBefore(trailer,new_name,new_url,new_type);
-    }
-
-    void printList(){             // Function to print entire list
-        Website* curWebsite = header->next;
-        std::cout << "\n";
-        std::cout << "PRINTING LIST... \n\n";
-        std::cout.width(20); std::cout << std::left << "Name";
-        std::cout.width(80); std::cout << std::left << "URL";
-        std::cout.width(20); std::cout << std::left << "Resource type";
-        std::cout.width(120); 
-        std::cout << "\n=============================================================================================================================\n";
-        int i = 0;
-        while (curWebsite != trailer){
-            std::cout << i << std::endl;
-            i++;
-            std::cout.width(20); std::cout << std::left << curWebsite->name;
-            std::cout.width(80); std::cout << std::left << curWebsite->url;
-            std::cout.width(20); std::cout << std::left << curWebsite->type;
+        void printList(){             // Function to print entire list
+            Node<T> *curNode = header->next;
             std::cout << "\n";
-            curWebsite = curWebsite->next;
+            std::cout << "PRINTING LIST... \n\n";
+            std::cout.width(20); std::cout << std::left << "Name";
+            std::cout.width(80); std::cout << std::left << "URL";
+            std::cout.width(20); std::cout << std::left << "Resource type";
+            std::cout << "\n===========================================================================================================================\n";
+            while (curNode != trailer){
+                std::cout.width(20); std::cout << std::left << curNode->data.name;
+                std::cout.width(80); std::cout << std::left << curNode->data.url;
+                std::cout.width(20); std::cout << std::left << curNode->data.type;
+                std::cout << "\n";
+                curNode = curNode->next;
+            }
+
+            std::cout << "\n";
         }
-
-        std::cout << "\n";
-    }
-
+        
 };
 
-LinkedList readFile(std::string filename){ // this, or one of the add functions is causing a duplicate error. see below
-    std::ifstream infile;
-    infile.open(filename);
-
-    LinkedList list;
-    std::string name;
-    std::string url;
-    std::string type;
-
-    int i = 0;
-    if (infile){ // its either here or in the add function(s) that there's a problem
-        // that causes a duplicate of the last item in the file to be added to the list
-        while (!infile.eof()){
-            infile >> name;
-            infile >> url;
-            infile >> type;
-            list.addBack(name,url,type);
-        }     
-    }
-
-    infile.close();
-    return list;
-}
 
 // Main function
 int main(){
-
-    LinkedList myLinkedList = readFile("exampleWebsites.txt");
-    myLinkedList.printList();
-    std::cout << "Count of myLinkedList: " << myLinkedList.returnSize();
-    std::cout << std::endl;
+    // Testing all functions of the list
+    
+    /*
+    Website site = Website("geeksforgeeks","https://www.geeksforgeeks.org/c-plus-plus/?ref=shm","Guide");
+    Node<Website> node = Node<Website>(site);
+    LinkedList<Website> list;
+    list.addFront(site);
+    list.printList();
+    site = Website("w3schools", "https://www.w3schools.com/cpp/" ,"Guide");
+    list.addFront(site);
+    list.printList();
+    list.removeFront();
+    list.printList();
+    list.addBack(site);
+    list.printList();
+    list.removeBack();
+    list.printList();
+    */
 
     return EXIT_SUCCESS;
 }
